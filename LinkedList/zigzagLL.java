955d0ce5e53579b257eb5ceb442784a424c7fccd
ananya pati <93138466+ananyapati888@users.noreply.github.com>
2024-02-28 07:29:06 +0000

    zigzagLL

---
+++ LinkedList/zigzagLL.java
@@ -0,0 +1,125 @@
+package LinkedList;
+public class zigzagLL {
+
+    static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static Node head;
+    public static Node tail;
+
+    public void addFirst(int data) {
+        Node newNode = new Node(data);
+
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+        newNode.next = head;
+        head = newNode;
+    }
+
+    public void addLast(int data) {
+        Node newNode = new Node(data);
+
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+
+        tail.next = newNode;
+        tail = newNode;
+    }
+
+    public void add(int idx, int data) {
+        if (idx == 0) {
+            addFirst(data);
+            return;
+        }
+
+        Node newNode = new Node(data);
+
+        Node temp = head;
+        int i = 0;
+        while (i < idx - 1) {
+            temp = temp.next;
+            i++;
+        }
+        newNode.next = temp.next;
+        temp.next = newNode;
+    }
+
+    public void zigzag() {
+
+        // find mid
+        Node slow = head;
+        Node fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        Node mid = slow;
+        Node curr = mid.next;
+        mid.next = null;
+
+        // reverse 2nd part
+        Node prev = null;
+        Node next;
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        Node left = head;
+        Node right = prev;
+        Node nLeft, nRight;
+
+        while (left != null && right != null) {
+            nLeft = left.next;
+            left.next = right;
+            nRight = right.next;
+            right.next = nLeft;
+
+            right = nRight;
+            left = nLeft;
+        }
+    }
+
+    public void print() {
+        if (head == null) {
+            System.out.println("the LL is empty");
+            return;
+        }
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String args[]) {
+
+        zigzagLL ll = new zigzagLL();
+
+        ll.addFirst(6);
+        ll.addFirst(5);
+        ll.addFirst(4);
+        ll.addFirst(3);
+        ll.addFirst(2);
+        ll.addFirst(1);
+
+        ll.zigzag();
+        ll.print();
+    }
+
+}
